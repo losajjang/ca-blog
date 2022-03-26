9b12033e7f888dc74388057e90ff48bc220a8cc3
losajjang <losajjang@naver.com>
2022-03-26 19:26:07 +0900

    지우고 처음부터 혼자 만들어봄

--- src/App.js
+++ src/App.js
@@ -3,46 +3,40 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동 맛집", "새해 해돋이 명소 추천"]);
-  let [하트, 하트변경] = useState(0);
-  let [modal, modal변경] = useState(false);
-  let posts = "강남 고기 맛집";
-  let [누른제목, 누른제목변경] = useState(0)
+  let [글제목, 글제목변경] = useState(["5x5 루틴", "8x8 루틴", "초보자 루틴"]);
+  let [발행일, 발행일변경] = useState(['2021.03.24','2021.03.25','2021.03.26'])
+  let [상세내용, 상세내용변경] = useState(['운동내용1','운동내용2','운동내용3'])
+  let [현재하트, 하트변경] = useState(0);
+  let [modal상태, modal상태변경] = useState(false)
+  let [제목클릭, 제목클릭변경] = useState(0)
 
   return (
     <div className="App">
-      <div className="black-nav">
-        <div>개발 Blog</div>
-      </div>
-     
-      {글제목.map(function (글,i) {
+      <h2 className="black-nav">개발 Blog</h2>
+      {글제목.map(function (글, i) {
         return (
           <div className="list">
-            <h4 onClick={()=>{누른제목변경(i)}}>{글}<span onClick={(a, i) => {하트변경(하트 + 1);}}>❤️</span>{하트}</h4>
-            <p>2월 17일 발행</p>
+            <h3 onClick={()=>{제목클릭변경(i)}}>{글}<span onClick={() => {하트변경(현재하트 + 1);}}>❤️</span>{현재하트}</h3>
+            <p>발행일: {발행일[i]}</p>
+            <p style={{display:"none"}}>{상세내용[i]}</p>
             <hr />
           </div>
         );
       })}
-
-      <button onClick={()=>{누른제목변경(0)}}>버튼1</button>
-      <button onClick={()=>{누른제목변경(1)}}>버튼2</button>
-      <button onClick={()=>{누른제목변경(2)}}>버튼3</button>
-
-      <button onClick={() => {modal변경(!modal);}}>버튼</button>
-      {modal === true ? <Modal 글제목 = {글제목} 누른제목={누른제목}></Modal> : null}
+      <input/>
+      <button onClick={()=>{modal상태변경(!modal상태)}}>글보기</button>
+      {modal상태 === true ? <Modal 글제목={글제목} 제목클릭={제목클릭} 발행일={발행일} 상세내용={상세내용}></Modal> : null}
     </div>
   );
-}
-
-function Modal(props) {
-  return (
-    <div className="modal">
-      <h2>{props.글제목[props.누른제목]}</h2>
-      <p>날짜</p>
-      <p>상세내용</p>
-    </div>
-  );
+  function Modal(props) {
+    return (
+      <div className="modal">
+        <h2>{props.글제목[props.제목클릭]}</h2>
+        <p>{props.발행일[props.제목클릭]}</p>
+        <p>{props.상세내용[props.제목클릭]}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
