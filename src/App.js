6fcd85ff75c40313f98af12b1d088dd88cf3cfbd
losajjang <losajjang@naver.com>
2022-03-26 17:13:02 +0900

    2nd commit

--- src/App.js
+++ src/App.js
@@ -7,72 +7,42 @@ function App() {
   let [하트, 하트변경] = useState(0);
   let [modal, modal변경] = useState(false);
   let posts = "강남 고기 맛집";
-
-  function 반복된ui() {
-    var 어레이 = [];
-    for (var i = 0; i < 3; i++) {
-      어레이.push(<div>안녕</div>);
-    }
-
-    return 어레이;
-  }
+  let [누른제목, 누른제목변경] = useState(0)
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      {반복된ui()}
-
-      {글제목.map(function (글) {
+     
+      {글제목.map(function (글,i) {
         return (
           <div className="list">
-            <h4>
-              {글}
-              <span
-                onClick={() => {
-                  하트변경(하트 + 1);
-                }}
-              >
-                ❤️
-              </span>
-              {하트}
-            </h4>
+            <h4 onClick={()=>{누른제목변경(i)}}>{글}<span onClick={(a, i) => {하트변경(하트 + 1);}}>❤️</span>{하트}</h4>
             <p>2월 17일 발행</p>
             <hr />
           </div>
         );
       })}
 
-      <button
-        onClick={() => {
-          modal변경(!modal);
-        }}
-      >
-        버튼
-      </button>
-      {modal === true ? <Modal /> : null}
+      <button onClick={()=>{누른제목변경(0)}}>버튼1</button>
+      <button onClick={()=>{누른제목변경(1)}}>버튼2</button>
+      <button onClick={()=>{누른제목변경(2)}}>버튼3</button>
+
+      <button onClick={() => {modal변경(!modal);}}>버튼</button>
+      {modal === true ? <Modal 글제목 = {글제목} 누른제목={누른제목}></Modal> : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h2>제목</h2>
+      <h2>{props.글제목[props.누른제목]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
   );
 }
-
-// function BtnWriteCancel(props) {
-//   return (
-//     <div>
-//       <button onClick={()=>{props.modal변경(true)}}>글작성</button>
-//       <button onClick={()=>{props.modal변경(false)}}>취소</button>
-//     </div>
-//   );
-// }
 
 export default App;
